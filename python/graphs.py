12338f80779c03dabbe5a4ac71a434eed20f8ae0
Princy Razafimanantsoa <princy.raza@gmail.com>
2018-09-13 06:39:23 +0000

    pycode lint

--- python/graphs.py
+++ python/graphs.py
@@ -1,22 +1,28 @@
 from collections import deque
 
+
 def bfs(graph, start=0):
-    to_visit = deque() # queue of nodes to visit
-    dist = [float('inf')] * len(graph) # init array for distances between start and each nodes of the graph: all infinity
-    prec = [None] * len(graph) # init array for predecesor of each node on the path: start -> node
-    dist[start] = 0 # distance from start to start = 0
+    to_visit = deque()  # queue of nodes to visit
+    # init array for distances between start and each nodes of the graph: all infinity
+    dist = [float('inf')] * len(graph)
+    # init array for predecesor of each node on the path: start -> node
+    prec = [None] * len(graph)
+    dist[start] = 0  # distance from start to start = 0
     to_visit.appendleft(start)
-    while to_visit: # empty queue returns false
+    while to_visit:  # empty queue returns false
         node = to_visit.pop()
         for neighbor in graph[node]:
-            if dist[neighbor] == float('inf'): # if distance not evaluated yet i.e distance == infinity
+            # if distance not evaluated yet i.e distance == infinity
+            if dist[neighbor] == float('inf'):
                 dist[neighbor] = dist[node] + 1
                 prec[neighbor] = node
                 to_visit.appendleft(neighbor)
     return dist, prec
 
+
 def main():
     print("Graph based algorithms")
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
